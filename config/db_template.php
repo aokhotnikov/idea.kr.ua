8762cd8aef2c2645579e234948466b60252b7a8f
aokhotnikov <hunterov1984@gmail.com>
2016-07-08 17:23:37 +0300

    add db_template

---
+++ config/db_template.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'class' => 'yii\db\Connection',
+    'dsn' => 'mysql:host=localhost;dbname=idea_kr_ua',
+    'username' => 'root',
+    'password' => 'fbi',
+    'charset' => 'utf8',
+];
