349b97c701ce43aac44e92cbe7019be706483e51
shpp-eganusyak <g_e_v@mail.ua>
2016-07-08 17:26:44 +0300

    delete db.php

--- config/db.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-return [
-    'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=idea_kr_ua',
-    'username' => 'root',
-    'password' => 'fbi',
-    'charset' => 'utf8',
-];
