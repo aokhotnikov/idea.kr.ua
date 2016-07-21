a0f02d99a53a3e0953e9c83edb6bad6ee18e07a2
aokhotnikov <hunterov1984@gmail.com>
2016-07-21 10:25:20 +0300

    add votes(files)

---
+++ models/Votes.php
@@ -0,0 +1,79 @@
+<?php
+
+namespace app\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "votes".
+ *
+ * @property integer $id
+ * @property integer $post_id
+ * @property integer $user_id
+ * @property integer $like_id
+ *
+ * @property Likes $like
+ * @property Posts $post
+ * @property Users $user
+ */
+class Votes extends \yii\db\ActiveRecord
+{
+    /**
+     * @inheritdoc
+     */
+    public static function tableName()
+    {
+        return 'votes';
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function rules()
+    {
+        return [
+            [['post_id', 'user_id', 'like_id'], 'required'],
+            [['post_id', 'user_id', 'like_id'], 'integer'],
+            [['like_id'], 'exist', 'skipOnError' => true, 'targetClass' => Likes::className(), 'targetAttribute' => ['like_id' => 'id']],
+            [['post_id'], 'exist', 'skipOnError' => true, 'targetClass' => Posts::className(), 'targetAttribute' => ['post_id' => 'id']],
+            [['user_id'], 'exist', 'skipOnError' => true, 'targetClass' => Users::className(), 'targetAttribute' => ['user_id' => 'id']],
+        ];
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'post_id' => 'Post ID',
+            'user_id' => 'User ID',
+            'like_id' => 'Like ID',
+        ];
+    }
+
+    /**
+     * @return \yii\db\ActiveQuery
+     */
+    public function getLike()
+    {
+        return $this->hasOne(Likes::className(), ['id' => 'like_id']);
+    }
+
+    /**
+     * @return \yii\db\ActiveQuery
+     */
+    public function getPost()
+    {
+        return $this->hasOne(Posts::className(), ['id' => 'post_id']);
+    }
+
+    /**
+     * @return \yii\db\ActiveQuery
+     */
+    public function getUser()
+    {
+        return $this->hasOne(Users::className(), ['id' => 'user_id']);
+    }
+}
